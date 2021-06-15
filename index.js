58ffc756465eed94dba0147c1390824dd03da1e0
Alex Chu <alex.chu16@hotmail.com>
2021-06-15 00:18:59 -0400

    finished mission statement section

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const more = document.querySelector(".more");
 const dropdown = document.querySelector('.dropdown');
-
+let opened = false;
 
 more.addEventListener('click', () => {
     dropdown.classList.toggle('open');
